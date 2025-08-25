c59bbc1a6cb2c07abe89ae01bda4b94ac9cb66f4
이종원 <whddnjs5255@gmail.com>
2025-08-25 18:55:48 +0900

    first commit

---
+++ src/test/java/study/synchronicity/SynchronicityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.synchronicity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronicityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
